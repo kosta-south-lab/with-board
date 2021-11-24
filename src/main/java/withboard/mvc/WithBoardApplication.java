eaf9b4e1c0666e0221a5997a5449593ff49c7601
Changwan Chae <69083280+chaechangwan@users.noreply.github.com>
2021-11-24 10:39:27 +0900

    initial-commit

---
+++ src/main/java/withboard/mvc/WithBoardApplication.java
@@ -0,0 +1,13 @@
+package withboard.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/withboard/mvc/WithBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package withboard.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
