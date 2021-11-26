5bfc04255d29c759b6688049756d2553b3398ff9
yunsol12345 <88135129+yunsol12345@users.noreply.github.com>
2021-11-26 14:01:11 +0900

    commit

---
+++ src/main/java/withboard/mvc/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package withboard.mvc.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
---
+++ src/main/java/withboard/mvc/controller/MemberController.java
@@ -0,0 +1,22 @@
+package withboard.mvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import withboard.mvc.service.MemberService;
+
+@Controller
+public class MemberController {
+	@Autowired 
+	private MemberService memberService;
+	
+	//회원가입
+	
+	
+	//로그인
+	
+	//로그아웃
+	
+	
+	
+}
