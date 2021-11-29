9c760a06be40459c6c9b7dd19a08d2e36e21be95
threebears00 <77993864+threebears00@users.noreply.github.com>
2021-11-29 14:36:37 +0900

    보드게임 도메인 및 한줄평 도메인 추가
     Game 내 gameDetail 추가
    
    GameComment 생성

---
+++ src/main/java/withboard/mvc/domain/GameComment.java
@@ -0,0 +1,46 @@
+package withboard.mvc.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GameComment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "game_comment_seq")
+	@SequenceGenerator(sequenceName = "game_comment_seq", allocationSize = 1, name = "game_comment_seq")
+	private Long commentNo;
+
+	@Column(length = 1000)
+	private String comment;
+
+	@CreationTimestamp
+	private LocalDateTime commentDate;
+
+	@ManyToOne
+	@JoinColumn(name = "game_no") // Reply테이블에 free_bno라를 필드 추가되고 fk설정
+	private Game game;
+
+}
